7f73efa8ef0addbfbe83174f84f5d137dcb4cab0
Tarush Agarwal <tarushagarwal2003@gmail.com>
2025-11-15 18:51:36 +0530

    minor navbar changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ReactNode } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
